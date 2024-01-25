41efd541cae3174df6b47aabf98bf85b80fa7025
Okpara007 <chinemeremokpara93@gmail.com>
2024-01-25 08:53:06 +0100

    updated allowed hosts

--- abn/settings.py
+++ abn/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = "django-insecure-o(o9r)30-!(==cw8o30zfe3)p6wh7o#0d79^n7@v!$8&cs8z8t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'https://abnrealtors.onrender.com',
-    ]
+ALLOWED_HOSTS = ['abnrealtors.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
