bdb7037a379d189148381c940588602b2831dc19
Okpara007 <chinemeremokpara93@gmail.com>
2024-05-18 23:48:56 +0100

    added host

--- abn/settings.py
+++ abn/settings.py
@@ -26,11 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# db8145e2d89f48d2e0a8abe078b7e9d7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ['abn-realtors.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['abn-realtors.onrender.com', 'localhost', '127.0.0.1', 'www.abnrealtorsgroup.com']
 
 
 # Application definition
